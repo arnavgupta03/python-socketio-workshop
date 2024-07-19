6a8771d730b87937a9405b4cab804c5e9aebbf1f
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2024-07-19 01:02:29 -0400

    add basic Flask server with Socket.IO

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "starterhacks"
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+@app.route("/", methods=['GET'])
+def home():
+    return render_template("index.html")
+
+
+@socketio.on("connect")
+def connect():
+    print("Connected!")
+
+
+# TODO: add method to catch messages sent to connected user
+
+# BONUS TODO: add method to join user to room
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
